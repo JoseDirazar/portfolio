436e83770a0fa9134f667f78e8a64d751c6f9422
Jo <jose_f_dirazar@hotmail.com>
2023-09-08 18:41:35 -0300

    fixes

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,7 +64,7 @@ export default function Intro() {
       >
      <span className="font-bold">Hello, I&apos;m Joseph.</span> I&apos;m a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">scrumm</span> experience. I enjoy building{" "}
+        <span className="font-bold">scrum</span> experience. I enjoy building{" "}
         <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
       </motion.h1>
