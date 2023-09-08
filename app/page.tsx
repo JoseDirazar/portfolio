40c37a1a571ed1a321476f0095a06d453a781cf6
Jo <jose_f_dirazar@hotmail.com>
2023-09-08 14:49:28 -0300

    about

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import About from "@/components/about";
+import Intro from "@/components/intro";
+import SectionDivider from "@/components/section-divider";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex flex-col items-center" >
+      <Intro />
+      <SectionDivider />
+      <About />
+    </main>
   );
 }
+/* className="flex min-h-screen flex-col items-center justify-between p-24" */
