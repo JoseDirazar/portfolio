cfc9a46e55d83828f30c6357608b6c672418b9d4
Jose Dirazar <jose_f_dirazar@hotmail.com>
2025-05-02 04:51:39 -0300

    Add metadata

--- app/[locale]/jose-dirazar/portfolio/layout.tsx
+++ app/[locale]/jose-dirazar/portfolio/layout.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Jose Dirazar | Portfolio",
+  description: "Jose Dirazar Portfolio",
+};
+
 export default function LocaleLayout({
   children,
 }: {
